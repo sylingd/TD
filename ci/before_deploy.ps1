a7162d5292463d4d99e01b31d0a72811713dc2f9
泷涯 <896640547@qq.com>
2019-03-13 01:07:36 +0800

    Update artifact [skip travis-ci]

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -19,12 +19,15 @@ foreach ($Bin in $Bins) {
 	Copy-Item "$SRC_DIR\target\$($Env:TARGET)\debug\$Bin.exe" ".\$($Env:PACKAGE_NAME)-DEBUG\"
 }
 
-7z a "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:TARGET)-RELEASE.zip" "$($Env:PACKAGE_NAME)-RELEASE\"
-7z a "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:TARGET)-DEBUG.zip" "$($Env:PACKAGE_NAME)-DEBUG\"
+7z a "$SRC_DIR\$($Env:PACKAGE_NAME)-$($Env:TARGET)-RELEASE.zip" "$($Env:PACKAGE_NAME)-RELEASE\"
+7z a "$SRC_DIR\$($Env:PACKAGE_NAME)-$($Env:TARGET)-DEBUG.zip" "$($Env:PACKAGE_NAME)-DEBUG\"
 
 Remove-Item *.* -Force
 Set-Location ..
 Remove-Item $STAGE -Force -Recurse
 Set-Location $SRC_DIR
 
-git tag $Env:RELEASE_NAME
+Push-AppveyorArtifact "$($Env:PACKAGE_NAME)-$($Env:TARGET)-RELEASE.zip"
+Push-AppveyorArtifact "$($Env:PACKAGE_NAME)-$($Env:TARGET)-DEBUG.zip"
+
+git tag "$($Env:RELEASE_NAME)"
